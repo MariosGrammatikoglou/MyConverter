6638e7d402948b9c6382236ccd134b12daa5b028
MAO\maogr <grammamarios@outlook.com>
2024-10-14 03:30:17 +0300

    Image-Preview

--- script.js
+++ script.js
@@ -2,44 +2,44 @@ const display = document.getElementById('display');
 const text = document.getElementById('textArea');
 
 
+
 function encode(){
     const result1 = btoa(text.value);
-    let result2 = result1.replace("Mzky","mao");
-    result2 = result2.replace("a","KilLj0y");
-    result2 = result2.replace("b","0meN21");
-    result2 = result2.replace("c","G3kK0me");
-    result2 = result2.replace("d","vAlOra5t");
-    result2 = result2.replace("e","Al1r860t");
-    result2 = result2.replace("f","Tr1pwlr3");
-    result2 = result2.replace("W","m4Od1dIT");
-    result2 = result2.replace("X","My3nc0d3r");
-    result2 = result2.replace("Y","H3ll0W0rld");
-    result2 = result2.replace("Z","Pr0gr4mm1ng");
-    result2 = result2.replace("w","c4phrC4ge");
-    result2 = result2.replace("x","enc0dH4pPnd");
-    result2 = result2.replace("y","c0py7ha7");
-    result2 = result2.replace("z","H1sP4c3");
-    result2 = result2.replace("==","1s0nx2");
+        let result2 = result1.replace("Mzky","mao");
+        result2 = result2.replace("p","KilLj0y");
+        result2 = result2.replace("q","0meN");
+     result2 = result2.replace("Q","G3kK0");
     display.value= result2;
 }
 
 function decode(){
     const result1 = text.value;
-    let result2 = result1.replace("Mzky","mao");
-    result2 = result2.replace("KilLj0y","a");
-    result2 = result2.replace("0meN21","b");
-    result2 = result2.replace("G3kK0me","c");
-    result2 = result2.replace("vAlOra5t","d");
-    result2 = result2.replace("Al1r860t","e");
-    result2 = result2.replace("Tr1pwlr3","f");
-    result2 = result2.replace("m4Od1dIT","W");
-    result2 = result2.replace("My3nc0d3r","X");
-    result2 = result2.replace("H3ll0W0rld","Y");
-    result2 = result2.replace("Pr0gr4mm1ng","Z");
-    result2 = result2.replace("c4phrC4ge","w");
-    result2 = result2.replace("enc0dH4pPnd","x");
-    result2 = result2.replace("c0py7ha7","y");
-    result2 = result2.replace("H1sP4c3","z");
-    result2 = result2.replace("1s0nx2","==");
+    let result2 = result1.replace("mao","Mzky");
+            result2 = result2.replace("KilLj0y","p");
+            result2 = result2.replace("0meN","q");
+            result2 = result2.replace("G3kK0","Q");
     display.value= atob(result2);
 }
+
+
+let toastBox = document.getElementById('toastBox');
+        
+        let encodeMsg ='<i class="fa-solid fa-lock"></i> Succesfully encoded!';
+        let decodeMsg ='<i class="fa-solid fa-unlock"></i> Succesfully decoded!';
+        let errorMsg ='<i class="fa-solid fa-keyboard"></i> Input not found!';
+        
+        function showToast(msg){
+            if(text.value.length == 0){
+                msg=errorMsg;
+            }
+                   
+                let toast = document.createElement('div');
+                toast.classList.add('toast');
+                toast.innerHTML = msg;
+                toastBox.appendChild(toast);
+            
+                setTimeout(()=>{
+                        toast.remove();
+                },6000);
+        }
+        
